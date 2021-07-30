f49a0b6ef90c68e55682f6ffabc7dc8c3f51fd6a
UserKen-first <dailyuseken@gmail.com>
2021-07-30 13:14:05 +0800

    建立loggers方法

--- WebDB/AccountingNote.DBSource/logger.cs
+++ WebDB/AccountingNote.DBSource/logger.cs
@@ -13,7 +13,17 @@ namespace AccountingNote.DBSource
             string msg = $@"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}
                         {ex.ToString()}
                 ";
-            System.IO.File.AppendAllText("C:\\Logs\\Log.log", ex.ToString());
+
+            string logPath = ("C:\\Logs\\Log.log");
+            string folderPath = System.IO.Path.GetDirectoryName(logPath);
+
+            if (!System.IO.Directory.Exists(folderPath))
+                System.IO.Directory.CreateDirectory(folderPath);
+
+            if (!System.IO.File.Exists(logPath))
+                System.IO.File.Create(logPath);
+            
+            System.IO.File.AppendAllText(logPath, msg);
 
             throw ex;
         }
